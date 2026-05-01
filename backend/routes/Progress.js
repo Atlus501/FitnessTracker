4b9e969ad106b9f8974a4c21d6dc12c613eda962
Atlus501 <helloworld4846@gmail.com>
2026-05-01 15:43:39 -0700

    small adjustments

--- backend/routes/Progress.js
+++ backend/routes/Progress.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 const helper = require('./helpers/ProgressHelper.js');
 
-//this function gets the progress of the users
-router.get("/:user_id", helper.getProgress);
-
 //this function udpates the progress of the goals
 router.post("/updateProgress", helper.updateProgress);
+
+//this function gets the progress of the users
+router.get("/:user_id", helper.getProgress);
 
 module.exports = router;
